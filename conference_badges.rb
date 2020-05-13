69dcf1fc2f40eacf810eb9a3f7a12724ac5fb26a
Camiyah Goins <tokyonote9@gmail.com>
2020-05-13 17:48:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  "Hello, my name is #{name}." #the arguem
 end
 
 def batch_badge_creator(speakers)
